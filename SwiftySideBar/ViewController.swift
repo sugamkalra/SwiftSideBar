6d5ecf152001b60d1a1f567437ca113f64c6c3d9
Sugam Kalra <sugamkalra@Sugams-MacBook-Pro.local>
2015-09-29 12:28:24 +0530

    First Commit

--- SwiftySideBar/ViewController.swift
+++ SwiftySideBar/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var menuLeftButton: UIButton!
+    
+    @IBOutlet weak var menuRightButton: UIButton!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if self.revealViewController() != nil
+        {
+            
+            
+            menuLeftButton.addTarget(self.revealViewController(), action: "revealToggle:", forControlEvents: UIControlEvents.TouchUpInside)
+            
+             menuRightButton.addTarget(self.revealViewController(), action: "rightRevealToggle:", forControlEvents: UIControlEvents.TouchUpInside)
+            
+
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+            // Uncomment to change the width of menu
+            //self.revealViewController().rearViewRevealWidth = 278
+        }
+
+        
     }
 
     override func didReceiveMemoryWarning() {
